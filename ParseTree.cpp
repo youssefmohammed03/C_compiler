2f2c98686d79124c44c3085c5b627e2567473954
Youssef Mohamed <21P0094@eng.asu.edu.eg>
2024-05-08 00:12:16 +0300

    parse tree

--- ParseTree.cpp
+++ ParseTree.cpp
@@ -23,31 +23,31 @@ public:
     }
 
     void printTree(int level = 0) {
-        // Print indentation
-        for (int i = 0; i < level; i++) {
-            std::cout << "  ";
-        }
-
-        // Print node type
-        std::cout << type;
-
-        // Print children
-        if (!children.empty()) {
-            std::cout << " -> [";
-            for (size_t i = 0; i < children.size(); i++) {
-                std::cout << children[i]->type;
-                if (i != children.size() - 1) {
-                    std::cout << ", ";
-                }
-            }
-            std::cout << "]";
-        }
-
-        std::cout << std::endl;
-
-        // Recursively print children's trees
-        for (ParseTreeNode* child : children) {
-            child->printTree(level + 1);
-        }
+    // Print indentation
+    for (int i = 0; i < level; i++) {
+        std::cout << "  ";
     }
+
+    // Print node type
+    std::cout << type;
+
+    // Print children
+    if (!children.empty()) {
+        std::cout << " -> { ";
+        for (size_t i = 0; i < children.size(); i++) {
+            std::cout << children[i]->type;
+            if (i != children.size() - 1) {
+                std::cout << ", ";
+            }
+        }
+        std::cout << " }";
+    }
+
+    std::cout << std::endl;
+
+    // Recursively print children's trees
+    for (ParseTreeNode* child : children) {
+        child->printTree(level + 1);
+    }
+}
 };
